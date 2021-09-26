3d5487c7d477e0533e25e0a09f02bbc8c0a157d0
jpromanonet <programming@jpromano.net>
2021-09-26 14:01:29 -0300

    Almost done

--- handlers/commands/help.js
+++ handlers/commands/help.js
@@ -6,7 +6,7 @@ const { prefix } = require('../../config.json');
 module.exports = {
     name: 'help',
     description: 'List all available commands, or info about a specific command.',
-    aliases: ['commands'],
+    aliases: ['comandos', 'ayuda'],
     usage: '[command name]',
     cooldown: 5,
     execute(message, args) {
@@ -15,18 +15,18 @@ module.exports = {
 
         // Send help data about ALL commands
         if(!args.length) {
-            data.push('Here\'s a list of all my commands:');
+            data.push('Aca hay una lista de todos mis comandos: \n');
             data.push(commands.map(command => command.name).join('\n'));
-            data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+            data.push(`\nPodes enviar \`${prefix}help [comando]\` para obtener información espefica de un comando!`);
 
             return message.author.send(data, { split: true })
                 .then(() => {
                     if (message.channel.type === 'dm') return;
-                    message.reply('I\'ve sent you a DM with all my commands!');
+                    message.reply('Te envie un DM con toda mi lista de comandos =)!');
                 })
                 .catch(error => {
-                    console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-                    message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+                    console.error(`No pude enviar el DM de ayuda a ${message.author.tag}.\n`, error);
+                    message.reply('¿Tenes los DMs deshabilitados?');
                 });
         }
 
